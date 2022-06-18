7532ae4772c54b7440b23bd7a97403eb9ad95c10
rastenievodstvo <106918521+rastenievodstvo@users.noreply.github.com>
2022-06-18 15:22:28 +0200

    Update scriptclick.js

--- scriptclick.js
+++ scriptclick.js
@@ -12,22 +12,22 @@ function border(){
 	}
 };
 	
-	function setBorder(){
-		isClicked = true;
-		point.style.border = '2px solid black';
-		var myx = (parseInt(point.style.left,10) - Number(2)) + 'px';
-		var myy = (parseInt(point.style.top,10) - Number(2)) + 'px';
-		point.style.left=myx;
-		point.style.top=myy;
-	}
+function setBorder(){
+	isClicked = true;
+	point.style.border = '2px solid black';
+	var myx = (parseInt(point.style.left,10) - Number(2)) + 'px';
+	var myy = (parseInt(point.style.top,10) - Number(2)) + 'px';
+	point.style.left=myx;
+	point.style.top=myy;
+}
 	
 function unsetBorder(){
-		isClicked = false;
-		point.style.border = 'none';
-		var myx = (parseInt(point.style.left,10) + Number(2)) + 'px';
-		var myy = (parseInt(point.style.top,10) + Number(2)) + 'px';
-		point.style.left=myx;
-		point.style.top=myy;	
+	isClicked = false;
+	point.style.border = 'none';
+	var myx = (parseInt(point.style.left,10) + Number(2)) + 'px';
+	var myy = (parseInt(point.style.top,10) + Number(2)) + 'px';
+	point.style.left=myx;
+	point.style.top=myy;	
 };
 
 document.addEventListener('dblclick', function (e) {
