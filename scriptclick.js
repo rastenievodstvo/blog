f580f212a9395d62974dbe6e2155b89af9a677c5
rastenievodstvo <106918521+rastenievodstvo@users.noreply.github.com>
2022-06-18 14:14:47 +0200

    Create scriptclick.js

---
+++ scriptclick.js
@@ -0,0 +1,31 @@
+function divclick(){
+	console.log('divclick');
+	border();
+};
+
+function border(){
+	if(isClicked){
+		unsetBorder();
+
+	}else{
+		setBorder();
+	}
+};
+	
+	function setBorder(){
+		isClicked = true;
+		point.style.border = '2px solid black';
+		var myx = (parseInt(point.style.left,10) - Number(2)) + 'px';
+		var myy = (parseInt(point.style.top,10) - Number(2)) + 'px';
+		point.style.left=myx;
+		point.style.top=myy;
+	}
+	
+function unsetBorder(){
+		isClicked = false;
+		point.style.border = 'none';
+		var myx = (parseInt(point.style.left,10) + Number(2)) + 'px';
+		var myy = (parseInt(point.style.top,10) + Number(2)) + 'px';
+		point.style.left=myx;
+		point.style.top=myy;	
+};
