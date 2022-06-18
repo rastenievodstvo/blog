85c01c0acb670a9031ad852fa102d030a4844ede
rastenievodstvo <106918521+rastenievodstvo@users.noreply.github.com>
2022-06-18 16:31:10 +0200

    Update scriptclick.js

--- scriptclick.js
+++ scriptclick.js
@@ -3,31 +3,47 @@ function divclick(){
 	border();
 };
 
+function flowerclick(){
+    console.log('flowerclick');
+    //myfunction();
+    //border();
+    flowerborder();
+}
+
+function flowerborder(){
+        var el = document.getElementsByClassName('flower')[1];
+        el.style.border = '2px solid black';
+        //this.style.border = '2px solid black';
+}
+
 function border(){
 	if(isClicked){
 		unsetBorder();
-
 	}else{
 		setBorder();
 	}
 };
+
+
 	
 function setBorder(){
 	isClicked = true;
-	point.style.border = '2px solid black';
-	var myx = (parseInt(point.style.left,10) - Number(2)) + 'px';
-	var myy = (parseInt(point.style.top,10) - Number(2)) + 'px';
-	point.style.left=myx;
-	point.style.top=myy;
+    var el = document.getElementsByClassName('flower')[0];
+    el.style.border = '2px solid black';
+    var myx = (parseInt(el.style.left,10) - Number(2)) + 'px';
+    var myy = (parseInt(el.style.top,10) - Number(2)) + 'px';
+    el.style.left=myx;
+    el.style.top=myy;
 }
 	
 function unsetBorder(){
 	isClicked = false;
-	point.style.border = 'none';
-	var myx = (parseInt(point.style.left,10) + Number(2)) + 'px';
-	var myy = (parseInt(point.style.top,10) + Number(2)) + 'px';
-	point.style.left=myx;
-	point.style.top=myy;	
+    var el = document.getElementsByClassName('flower')[0];
+    el.style.border = 'none';
+    var myx = (parseInt(el.style.left,10) + Number(2)) + 'px';
+    var myy = (parseInt(el.style.top,10) + Number(2)) + 'px';
+    el.style.left=myx;
+    el.style.top=myy;
 };
 
 document.addEventListener('dblclick', function (e) {
