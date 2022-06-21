33f5dfd341219b050a16c8b5a03e194b1dbe2264
rastenievodstvo <106918521+rastenievodstvo@users.noreply.github.com>
2022-06-21 14:39:36 +0200

    Update key.js

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-document.addEventListener('keyup', function(event){
+document.addEventListener('keydown', function(){
     console.log('Key: ', event.key);
     console.log('keyCode: ', event.keyCode);
     if(event.key == 'ArrowLeft'){
@@ -41,11 +41,52 @@ document.addEventListener('keyup', function(event){
 });
 
 /*
-document.addEventListener('keydown', function(){
-    console.log('Success onkeydown');
-	
+document.addEventListener('keyup', function(event){
+    console.log('Key: ', event.key);
+    console.log('keyCode: ', event.keyCode);
+    if(event.key == 'ArrowLeft'){
+        console.log('left');
+	    if(isLogoClicked){
+            myleft();
+	    }
+    }
+    if(event.key == 'ArrowRight'){
+        console.log('right');
+	    if(isLogoClicked){
+            myright();
+	    }
+    }
+    if(event.key == 'ArrowDown'){
+        console.log('down');
+	    if(isLogoClicked){
+            mydown();
+	    }
+    }
+    if(event.key == 'ArrowUp'){
+        console.log('up');
+	    if(isLogoClicked){
+            mytop();
+	    }
+    }
+    if((event.key == 'c') || (event.key == 'с')){
+            console.log('c');
+            unsetBorderAll()
+    }
+    if(event.key == 'm'){
+            console.log('m');
+            alert("menu");
+            
+    }
+    if(event.key == 'i'){
+            console.log('i - info');
+            alert("info");
+    }
 });
 */
+
+
+
+/*
 document.addEventListener('keypress', function(event){
     console.log('Success onkeypress');
     console.log('Key: ', event.key);
@@ -67,7 +108,7 @@ document.addEventListener('keypress', function(event){
 	mytop();
     }
 });
-
+*/
 /*
 document.addEventListener('keyup', function(){
     console.log('Success onkeyup');
