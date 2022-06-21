b90fdb180d44db31345320fa731d76c409dd5bec
rastenievodstvo <106918521+rastenievodstvo@users.noreply.github.com>
2022-06-21 12:39:35 +0200

    Update mousejs.js

--- mousejs.js
+++ mousejs.js
@@ -1,27 +1,33 @@
 let isMoving = false;
-let x = 0;
-let y = 0;
+//let x = 0;
+//let y = 0;
 
 document.addEventListener('mousedown', e => {
 	console.log('mousedown');
 	if(isLogoClicked){
 		isMoving = true;
-  		x = e.offsetX;
-  		y = e.offsetY;
-	}/*else{
-		unsetBorder();	
-	}*/
+  		//x = e.offsetX;
+  		//y = e.offsetY;
+	}
 });
 
 document.addEventListener('mousemove', e => {
 	console.log('mousemove');
 	if(isLogoClicked){
 		if(isMoving){
-    		x = e.offsetX;
-    		y = e.offsetY;
             var el = document.getElementsByClassName('flower')[0];
-            el.style.left = x;
-            el.style.top = y;
+            //x = e.offsetX;
+    		//y = e.offsetY;
+            //el.style.left = x;
+            //el.style.top = y;
+            if((e.offsetX <= 10) || (e.offsetY <= 10)){
+                return false;
+            }else{
+                el.style.left = e.offsetX;
+                el.style.top = e.offsetY;
+                console.log('x = ', e.offsetX);
+                console.log('y = ', e.offsetY);
+            }
 		}
 	}
 });
@@ -30,9 +36,7 @@ document.addEventListener('mouseup', e => {
 	console.log('mouseup');
 	if(isLogoClicked){
 		isMoving = false;
-  		x = e.offsetX;
-  		y = e.offsetY;
-	}/*else{
-		unsetBorder();	
-	}*/
+  		//x = e.offsetX;
+  		//y = e.offsetY;
+	}
 });
