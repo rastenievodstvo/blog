6f720bf2715050cd3b9b61e820ecf4ecfdc36b0c
rastenievodstvo <106918521+rastenievodstvo@users.noreply.github.com>
2022-06-21 10:12:24 +0200

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,27 +1,39 @@
 function myleft(){
-	console.log(Number(point.style.left));
-	//square.style.left = (parseInt(square.style.left,10)+50)+'px';
-	var x = (parseInt(point.style.left,10) - Number(10)) + 'px';
-	//alert(x);
-	point.style.left=x;
+    var el = document.getElementsByClassName('logo')[0];
+	//console.log(Number(point.style.left));
+    console.log(Number(el.style.left));
+	//var x = (parseInt(point.style.left,10) - Number(10)) + 'px';
+	var x = (parseInt(el.style.left,10) - Number(10)) + 'px';
+	//point.style.left=x;
+    el.style.left=x;
 }
 
 function myright(){
-	console.log(Number(point.style.left));
-	//square.style.left = (parseInt(square.style.left,10)+50)+'px';
-	var x = (parseInt(point.style.left,10) + Number(10)) + 'px';
-	//alert(x);
-	point.style.left=x;
+    var el = document.getElementsByClassName('logo')[0];
+	//console.log(Number(point.style.left));
+    console.log(Number(el.style.left));
+	//var x = (parseInt(point.style.left,10) + Number(10)) + 'px';
+    var x = (parseInt(el.style.left,10) + Number(10)) + 'px';
+	//point.style.left=x;
+    el.style.left=x;
 }	
 
 function mytop(){
-	console.log(Number(point.style.top));
-	var y = (parseInt(point.style.top,10) - Number(10)) + 'px';
-	point.style.top=y;
+    var el = document.getElementsByClassName('logo')[0];
+	//console.log(Number(point.style.top));
+    console.log(Number(el.style.top));
+	//var y = (parseInt(point.style.top,10) - Number(10)) + 'px';
+    var y = (parseInt(el.style.top,10) - Number(10)) + 'px';
+	//point.style.top=y;
+    el.style.top=y;
 }
 
 function mydown(){
-	console.log(Number(point.style.top));
-	var y = (parseInt(point.style.top,10) + Number(10)) + 'px';
-	point.style.top=y;
+    var el = document.getElementsByClassName('logo')[0];
+	//console.log(Number(point.style.top));
+    console.log(Number(el.style.top));
+	//var y = (parseInt(point.style.top,10) + Number(10)) + 'px';
+    var y = (parseInt(el.style.top,10) + Number(10)) + 'px';
+	//point.style.top=y;
+    el.style.top=y;
 }
